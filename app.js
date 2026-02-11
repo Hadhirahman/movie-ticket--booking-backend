168fb922f8c92947a3e587bcb46c0e7897e7c32a
hadhirahman <hadhirahman@gmail.com>
2026-02-11 17:20:55 +0530

    .env updated

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express=require('express');
 const app=express()
-const port=3000;
+const port=process.env.PORT || 3000;
 const dotenv=require('dotenv');
 app.use(express.json());
 dotenv.config();
